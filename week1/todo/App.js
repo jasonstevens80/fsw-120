a5bfe16ad548c68b05746116d5c0c764ccf03d8a
jasonstevens80 <71710082+jasonstevens80@users.noreply.github.com>
2021-01-21 11:51:36 -0500

    Add files via upload

---
+++ week1/todo/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> To Do List</h1>
+      <input type="checkbox"/>
+      <p>Exercise dog</p>
+
+      <input type="checkbox"/>
+      <p>Clean House</p>
+
+      <input type="checkbox"/>
+      <p>Finish homework</p>
+
+      <input type="checkbox"/>
+      <p>Go to work</p>
+    </div>
+  );
+}
+
+export default App;
