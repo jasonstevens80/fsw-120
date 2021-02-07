9e26edb383b2ca69c3dcd5a736225a9ee9ed519f
Jason Stevens <71710082+jasonstevens80@users.noreply.github.com>
2021-02-07 09:02:20 -0500

    Add files via upload

---
+++ week3/dice-game/Components/Dice.js
@@ -0,0 +1,20 @@
+  
+import React from "react"
+
+function Dice(props) {
+    const style = {
+        border: "black solid 2px", 
+        textAlign: "center",
+        width: 120,
+        gridColumn: "span 1",
+        padding: 10,
+        
+    }
+    return (
+        <div style={style}>
+            <h1>{props.number}</h1>
+        </div>
+    )
+}
+
+export default Dice
