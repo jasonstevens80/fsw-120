58ecfb9b10bf21a867327e4cc186d6ef66509f40
Jason Stevens <71710082+jasonstevens80@users.noreply.github.com>
2021-02-21 17:59:56 -0500

    Add files via upload

---
+++ week5/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return ( 
+        <footer className="mt-3">
+            &copy; Roto-Router 2021
+        </footer>
+    );
+}
+
+export default Footer;
