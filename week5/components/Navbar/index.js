b57824db2ac23d6d12b322441884dd542ac56e7a
Jason Stevens <71710082+jasonstevens80@users.noreply.github.com>
2021-02-21 18:00:26 -0500

    Add files via upload

---
+++ week5/components/Navbar/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './styles.css';
+
+class NavBar extends React.Component {
+
+    render() {
+        return ( 
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/services">Services</Link>
+            </nav>
+        );
+    }
+}
+
+export default NavBar;
