7ab2e03aed8699177a9b0c2bc418eb6c4dc79cb6
Jason Stevens <71710082+jasonstevens80@users.noreply.github.com>
2021-02-28 14:42:38 -0500

    Add files via upload

---
+++ week6/Input.js
@@ -0,0 +1,14 @@
+  
+import React, { Component } from 'react'
+
+class Input extends Component {
+    render() {
+        return(
+            <div className='input'>
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
+export default Input
